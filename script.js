ee0185d998eba6bdc0118f478cee33ff29c36dad
Rojus Stoncius <90309780+Rojus24@users.noreply.github.com>
2021-09-15 08:04:35 +0000

    2 ballen die stuiteren

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-var x;
-var v;
-function setup() {
-	createCanvas(500, 400);
+var x, y, vx, vy
+var x2, y2, vx2, vy2
 
+function setup() {
+  createCanvas(500, 400);
+  
+  vx = 5;
+  vy = 5;
+  y = 200;
   x = 30;
-  v = 10;
+  vx2 = 5;
+  vy2 = 5;
+  y2 = 20;
+  x2 = 30;
 }
 
 
 
 function draw() {
-	background(225);
+  background(225);
 
-  fill ('red')
-	circle(x,130,20);
-
-  x = x + v;
-
-  if (x > 500) {
-    v = -5;
+  ellipse(x,y,50,50)
+  fill('red')
+  circle(x2,y2,25);
+  
+  x = x + vx;
+  y = y + vy;
+  
+  if(x < 0 || x > 500){
+    vx = vx * -1;
   }
-  if (x < 0) {
-    v = 5;
+
+  if(y < 0 || y > 400){
+    vy = vy * -1;
+  }
+  x2 = x2 + vx2;
+  y2 = y2 + vy2;
+  
+  if(x2 < 0 || x2 > 500){
+    vx2 = vx2 * -1;
   }
 
-
-
-
-
-
+  if(y2 < 0 || y2 > 400){
+    vy2 = vy2 * -1;
+  }
 }
